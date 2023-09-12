01c3f8def2ebe0dd915cb40d3bad2eb4c8c37adf
Adarsh Sarangi <adarshsarangi2@gmail.com>
2023-09-12 21:23:06 +0530

    chore: add form

--- components/Form.jsx
+++ components/Form.jsx
@@ -11,15 +11,34 @@ const Form = ({type, post, setPost, submitting , handleSubmit}) => {
       onSubmit={handleSubmit}
       className='mt-10 wt-full max-w-2xl flex flex-col gap-7 glassmorphism'
       >
+
+        
         <label><span className='font-satoshi font-semibold text-base text-gray-700'>Your AI prompt</span></label>
         <textarea
-        value = {post.prompt}
+          value = {post.prompt}
           onChange={(e)=>setPost({ ...post , prompt : e.target.value})}
           placeholder='write your prompt here'
           required
           className='form_textarea'
         >
+        </textarea> 
+        <label><span className='font-satoshi font-semibold text-base text-gray-700'>Tag<span className='font-normal'>(#product #webdevelopment #idea)</span></span></label>
+        <textarea
+          value = {post.prompt}
+          onChange={(e)=>setPost({ ...post , prompt : e.target.value})}
+          placeholder='#tag'
+          required
+          className='form_input'
+        >
         </textarea>
+        <div className='flex-end mx-3 mb-5 gap-4'>
+        <Link href="/" className='text-gray-500 text-sm'>
+        Cancel
+        </Link>
+        <button type="submit" disabled={submitting} className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'>
+        {submitting ? `${type}...` : type}
+        </button>
+        </div>
       </form>
     </section>
   )
